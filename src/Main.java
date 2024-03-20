7fa16adb2d1e0f2fcd4487cd0f0a3b4c557a5e8c
citrinitas <daniargizatullin8@gmail.com>
2024-03-20 23:58:49 +0500

    ДЗ строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника: " + fullName);
+        System.out.println("Task 2");
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта: " + fullName.toUpperCase());
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович".replace('ё' , 'е');
+        System.out.println("Данные Ф.И.О. сотрудника: " + fullName);
+
     }
 }
